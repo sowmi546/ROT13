be49c5000d35dc96b2fe0fb963b0b9d67d0a18f9
Sowmya Duggimpudi <sowmiduggimpudi@gmail.com>
2017-02-26 15:10:53 -0500

    ROT13

---
+++ main.py
@@ -0,0 +1,122 @@
+    # Copyright 2016 Google Inc.
+    #
+    # Licensed under the Apache License, Version 2.0 (the "License");
+    # you may not use this file except in compliance with the License.
+    # You may obtain a copy of the License at
+    #
+    #     http://www.apache.org/licenses/LICENSE-2.0
+    #
+    # Unless required by applicable law or agreed to in writing, software
+    # distributed under the License is distributed on an "AS IS" BASIS,
+    # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    # See the License for the specific language governing permissions and
+#    # limitations under the License.
+#
+#    import webapp2
+#
+#    class MainPage(webapp2.RequestHandler):
+#        def get(self):
+#            self.response.headers['Content-Type'] = 'text/plain'
+#            self.response.write('Hello, Madhu123!')
+#            self.response.write(form)
+#
+#
+#    app = webapp2.WSGIApplication([
+#        ('/', MainPage),
+#    ], debug=True)
+
+import os
+import webapp2
+import jinja2
+from string import maketrans
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+
+# hidden_html = """
+# <input type="hidden" name="food" value="%s">
+# """
+
+# item_html = "<li>%s</li>"
+
+# shopping_list_html = """
+# <br>
+# <br>
+# <h2>Shopping List </h2>
+# <ul>
+# %s
+# </ul>
+# """
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        # self.response.headers['Content-Type'] = 'text/plain'
+        items = self.request.get_all("food")
+
+        # self.render("shopping_list.html", name= self.request.get("name"))
+        self.render("shopping_list.html", items=items)
+
+
+        # output = form_html
+        # output_hidden = ""
+        # items = self.request.get_all("food")
+        # if items:
+        #     output_items = ""
+        #     for item in items:
+        #          output_hidden = output_hidden + hidden_html % item
+        #          output_items += item_html % item
+
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
+
+        # output = output % output_hidden
+        # self.write(output)
+
+# class TestHandler(webapp2.RequestHandler):
+#     def post(self):
+#         # q = self.request.get("q")
+#         # self.response.out.write(q)
+#         self.response.headers['Content-Type'] ='text/plain'
+#         self.response.out.write(self.request)
+class FizzBuzz(Handler):
+    def get(self):
+        # self.response.headers['Content-Type'] = 'text/plain'
+        n = self.request.get("n")
+        if n:
+            n = int(n)
+        # self.render("shopping_list.html", name= self.request.get("name"))
+
+
+        self.render("fizzbuzz.html",n = n)
+class Rot13(Handler):
+    def get(self):
+        # self.response.headers['Content-Type'] = 'text/plain'
+
+
+        # self.render("shopping_list.html", name= self.request.get("name"))
+        self.render("rotfile.html")
+    def post(self):
+        rot13 = ''
+        text= self.request.get('text')
+        if text:
+            rot13 = text.encode('rot13')
+        self.render('rotfile.html', text= rot13)
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/fizzbuzz', FizzBuzz),
+                               ('/rot13', Rot13)
+                               ],
+                              debug=True)
